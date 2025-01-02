d9178c82319436f60c4242d6cd0405c11bb07bb6
faz <vantevivid@gmail.com>
2025-01-02 12:14:33 +0800

    Create header.php

---
+++ CRUD Update/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>adding products</title>
+
+</head>
+<body>
+    <header>
+        <div class="flex">
+           <a href="" class="logo"> Product Catalog </a>
+            <div class="navbar">
+                <a href="dashboardAdmin.html">Home</a>
+                <a href="admin.php">view products</a>
+                <a href="viewCatalog.php">Catalog</a>
+                <a href="loginAdmin.html">Logout</a>
+
+
+            </div>
+            
+
+        </div>
+
+    </header>
+
+</body>
+
+
+</html>
